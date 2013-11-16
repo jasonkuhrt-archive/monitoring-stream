bb0a3e514f6eb1f9f9af2acd19a37102b8694653
Jason Kuhrt <jasonkuhrt@me.com>
2013-11-15 21:22:12 -0500

    Improve event emission logic with help of maybe monad

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ var EventEmitter2 = require('EventEmitter2').EventEmitter2;
 var _ = require('lodash');
 var request = require('superagent');
 var log = require('debug')('uri_monitor');
+var Maybe = require('maybe');
+var state = require('./lib/state');
+var maybef = require('maybef');
+
+// Helpers
+
+function tap_compose(){
+  // instead of partial_right can we use curry1?
+  return compose.apply(null, map(arguments, function(f){ return partial_right(tap, f); } ));
+}
+
+var all = _.all;
+var partial = _.partial;
+var partial_right = _.partialRight;
+var map = _.map;
+var first = _.first;
+var tap = _.tap;
+var compose = _.compose;
+var to_array = _.toArray;
+var curry = _.curry;
+var not = function(a){ return !a; }
+
+
+
 
 
 
@@ -16,14 +40,14 @@ module.exports = function create_uri_monitor(uri, interval_ms){
 
 
   var api = new EventEmitter2();
-  var handle_ping_response = make_tracker(1, do_handle_ping_response);
+  var handle_ping_response = state(1, do_handle_ping_response);
 
   api.start = function(){
     log('start');
     var loop = function(){
       is_monitoring = setTimeout(do_ping, interval_ms);
     };
-    var do_ping = _.partial(api.ping, loop);
+    var do_ping = partial(api.ping, loop);
     do_ping();
   };
 
@@ -36,11 +60,8 @@ module.exports = function create_uri_monitor(uri, interval_ms){
   api.ping = function(callback){
     log('pinging', uri);
     var handle_response = function(err, res){
-      var any_error = err || res.error;
-      handle_ping_response(any_error ? false : true);
-      if (_.isFunction(callback)) {
-        callback(err, res);
-      }
+      handle_ping_response(err || res.error ? false : true);
+      maybef(callback)(err, res)
     };
 
     request(uri).end(handle_response);
@@ -54,41 +75,29 @@ module.exports = function create_uri_monitor(uri, interval_ms){
 
   var is_monitoring;
 
-  function do_handle_ping_response(response, history){
-    var response_type = response ? 'pong' : 'drop' ;
-    log('ping got ' + response_type);
-    var response_prev = _.first(history);
+  function do_handle_ping_response(is_pong, pong_history){
+    var maybe_connection_change = curry(function(is_pong, was_pong){
+      return Maybe(all([is_pong, not(was_pong)]) ? true  :
+                   all([not(is_pong), was_pong]) ? false :
+                   null);
+    });
 
-    // Emit an event for cases where the response_type differs from previous.
-
-    if (_.isUndefined(response_prev)){
-      // Always emit on the response of the first ping
-      api.emit({true:'connection', false:'disconnection'}[response]);
-    } else if (!response_prev && response) {
-      api.emit('connection');
-    } else if (response_prev && !response) {
-      api.emit('disconnection');
+    function io_handle_response(is_pong){
+      tap_compose(api.emit.bind(api), partial(log, 'ping got'))(is_pong ? 'pong' : 'drop');
     }
 
+    function io_handle_connection_change(to_connection){
+      tap_compose(api.emit.bind(api), partial(log, 'state change'))(to_connection ? 'connection' : 'disconnection');
+    }
+
+    // Emit response type
+    io_handle_response(is_pong);
+
+    // Emit connection-change type, if applicable
+    Maybe(first(pong_history))
+      .maybe(not(is_pong), maybe_connection_change(is_pong))
+      .bind(io_handle_connection_change);
   }
 
   return api;
 };
-
-
-// Helpers
-
-function tern(condition_f, true_f, false_f){
-  condition_f() ? true_f() : false_f() ;
-}
-
-function make_tracker(max_history_size, handler_f){
-  var history = [];
-  return function(value){
-    handler_f(value, _.clone(history));
-    history.unshift(value);
-    if (_.size(history) > max_history_size) {
-      history.pop();
-    }
-  }
-}
