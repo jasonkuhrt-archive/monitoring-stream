jasonkuhrt-archive/monitoring-stream

commit e029e84bad5da6a815e04b624ae2f70718ff45bf

Remove ping from the official API

index.js
@@ -75,16 +75,14 @@ module.exports = function create_uri_monitor(uri, interval_ms){
     }
   };
 
-  api.ping = function(callback){
+  api.ping = function(){
     log('pinging', uri);
     var handle_response = function(err, res){
       var any_error = err || res.error;
       state((any_error ? false : true), function(is_pong, pong_history){
         do_handle_ping_response(is_pong, first(pong_history), any_error || res);
       });
-      if (_.isFunction(callback)) callback(err, res) ;
     };
-
     _current_request = request(uri).timeout(2000).end(handle_response);
   };
 

commit 475b8d1a94262f0398d03187b1d9e7ade8ac4142

Fix regression caused by e029e84

index.js
@@ -75,14 +75,16 @@ module.exports = function create_uri_monitor(uri, interval_ms){
     }
   };
 
-  api.ping = function(){
+  api.ping = function(callback){
     log('pinging', uri);
     var handle_response = function(err, res){
       var any_error = err || res.error;
       state((any_error ? false : true), function(is_pong, pong_history){
         do_handle_ping_response(is_pong, first(pong_history), any_error || res);
       });
+      if (_.isFunction(callback)) callback(err, res) ;
     };
+
     _current_request = request(uri).timeout(2000).end(handle_response);
   };
 