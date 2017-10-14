e2414b03cd8c13c0bb5db170330af71d34bd5ba3
Jason Kuhrt <jason.kuhrt@ssense.com>
2017-10-14 00:00:49 -0400

    fix lint warnings

--- examples/request-retry.js
+++ examples/request-retry.js
@@ -1,13 +1,13 @@
 import Monitor from "../source/main"
 
 const action = () =>
-  Boolean(Math.round(Math.random()))
+  Math.round(Math.random())
     ? Promise.reject(new Error("This bad thing happened"))
     : Promise.resolve("OK!")
 
 const monitor = Monitor.create(action, 1000)
 
-const maxRetries = monitor.pongs
+monitor.pongs
   .takeUntil(monitor.drops.takeUntil(monitor.pongs).take(3))
   .take(1)
   .drain()
