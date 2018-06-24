62b2aea761d9c30adfcf2c89f527888a87a1445a
PrettierCI <commits@prettierci.com>
2018-06-24 01:20:50 +0000

    Sync with Prettier

--- source/main.js
+++ source/main.js
@@ -1,27 +1,27 @@
-import * as FRP from "most"
+import * as FRP from "most";
 
 const CheckEvent = (isResponsive, data, error) => ({
   isResponsive,
   data,
-  error,
-})
+  error
+});
 
 const createActionRunner = action => () => {
-  let result
+  let result;
   try {
     return action()
       .then(data => CheckEvent(true, data, null))
-      .catch(error => CheckEvent(false, null, error))
+      .catch(error => CheckEvent(false, null, error));
   } catch (error) {
-    return Promise.resolve(CheckEvent(false, null, error))
+    return Promise.resolve(CheckEvent(false, null, error));
   }
-}
+};
 
-const isDown = event => !event.isResponsive
-const isUp = event => event.isResponsive
-const isChange = event => event.isResponsiveChanged
-const isFall = event => !event.isResponsive && event.isResponsiveChanged
-const isRise = event => event.isResponsive && event.isResponsiveChanged
+const isDown = event => !event.isResponsive;
+const isUp = event => event.isResponsive;
+const isChange = event => event.isResponsiveChanged;
+const isFall = event => !event.isResponsive && event.isResponsiveChanged;
+const isRise = event => event.isResponsive && event.isResponsiveChanged;
 
 const create = (action, checkIntervalMs = 1000) => {
   const stream = FRP.periodic(checkIntervalMs)
@@ -32,19 +32,19 @@ const create = (action, checkIntervalMs = 1000) => {
     .skip(1)
     .map(([prev, curr]) =>
       Object.assign(curr, {
-        isResponsiveChanged: !prev || prev.isResponsive !== curr.isResponsive,
-      }),
+        isResponsiveChanged: !prev || prev.isResponsive !== curr.isResponsive
+      })
     )
-    .multicast()
+    .multicast();
 
   return Object.assign(stream, {
     downs: stream.filter(isDown),
     ups: stream.filter(isUp),
     changes: stream.filter(isChange),
     falls: stream.filter(isFall),
-    rises: stream.filter(isRise),
-  })
-}
+    rises: stream.filter(isRise)
+  });
+};
 
-export default { create, isDown, isUp, isChange, isFall, isRise }
-export { create, isDown, isUp, isChange, isFall, isRise }
+export default { create, isDown, isUp, isChange, isFall, isRise };
+export { create, isDown, isUp, isChange, isFall, isRise };
